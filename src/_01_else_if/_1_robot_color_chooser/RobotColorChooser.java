905dcf328035da2d95c9c6458d68da907ff3a437
student <student@ip-172-31-50-230.us-west-2.compute.internal>
2021-01-28 18:30:05 -0800

    h

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -12,6 +12,26 @@ public class RobotColorChooser {
 		//1. Create a new Robot\
 		Robot rob = new Robot();
 	    //2. Make the robot draw a shape (this will take more than one line of code)	
+		
+		//3. Set the pen width to 10
+		rob.setPenWidth(10);
+		//4. Ask the user what color pen they would like the robot to draw with
+	String color = JOptionPane.showInputDialog("What color would you like to draw with");
+		//5. Use an if/else statement to set the pen color that the user requested
+	if(color.equals("red")) {
+		rob.setPenColor(500,0,0);
+	}
+	else if(color.equals("green")) {
+		rob.setPenColor(0,500,0);
+	}
+	else if(color.equals("blue")) {
+		rob.setPenColor(0,0,500);
+	}
+        //6. If the user doesn't enter anything, choose a random color
+	else {
+		rob.setRandomPenColor();
+	}
+        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		rob.setSpeed(100);
 		rob.penDown();
 		rob.move(100);
@@ -21,15 +41,6 @@ public class RobotColorChooser {
 		rob.move(100);
 		rob.turn(90);
 		rob.move(100);
-		//3. Set the pen width to 10
-		rob.setPenWidth(10);
-		//4. Ask the user what color pen they would like the robot to draw with
-	String color = JOptionPane.showInputDialog("What color would you like to draw with");
-		//5. Use an if/else statement to set the pen color that the user requested
-	if()
-        //6. If the user doesn't enter anything, choose a random color
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
 
 		
 
--- src/_03_modulo/_2_bullseye/bullseye/bullseye.pde
+++ src/_03_modulo/_2_bullseye/bullseye/bullseye.pde
@@ -1,6 +1,6 @@
 
 void setup() {
- 
+ size(500,500);
   // set the size of your sketch
   
 }
@@ -8,8 +8,17 @@ void setup() {
 void draw() {
   
   //Starting with the largest ellipse, use a for loop to draw a bullseye with ellipses.
-  
+for(int ellipse = 400; ellipse > 0; ellipse -= 1){
+  if(ellipse % 100 == 0){
+   fill(500,0,0); 
+   ellipse(250,250,ellipse,ellipse);
+  }
+  if(ellipse % 100 == 50){
+    fill(0,0,0);
+    ellipse(250,250,ellipse,ellipse);
+  }
+    
+}
   //Use an if statement and modulo to alternate the color of your rings.
-  
   
 }
